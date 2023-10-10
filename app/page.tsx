6ac3a7991709ddc52a9a1c76444bc92c5c559917
JefersonCopini <116360705+JefersonCopini@users.noreply.github.com>
2023-10-10 23:54:37 +0000

    Fix: Add Banner, Header,Courses and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import Image from 'next/image'
+import Banner from "./components/Banner";
+import Courses from "./components/Courses";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
+
+
 
 export default function Home() {
   return (
-    <h1 className="text-center mt-8"> Escola tecnologica </h1>
+    <div>
+   <Header/>
+   <Banner />
+   <Courses />
+   <Footer />
+   
+   </div>
   )
 }
